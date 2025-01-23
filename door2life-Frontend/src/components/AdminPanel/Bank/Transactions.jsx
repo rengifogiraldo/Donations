4dbda79de88bd335f6fd56cec73cae89eaed6b48
rengifogiraldo <40236916+rengifogiraldo@users.noreply.github.com>
2025-01-24 02:08:37 +0500

    Update Transactions.jsx

--- door2life-Frontend/src/components/AdminPanel/Bank/Transactions.jsx
+++ door2life-Frontend/src/components/AdminPanel/Bank/Transactions.jsx
@@ -14,7 +14,7 @@ const Transactions = () => {
     const fetchTransactions = async () => {
       try {
         const response = await axios.get(
-          "https://door2life-backend.vercel.app/api/bank/main-bank"
+          "https://donations-prdd.onrender.com/api/bank/main-bank"
         );
         if (response.status === 200) {
           setUser(response.data);
