5eb5311262f68e31acf39645deafb3dcf77faa30
rengifogiraldo <40236916+rengifogiraldo@users.noreply.github.com>
2025-01-24 01:03:01 +0500

    Update server.js

--- door2life-backend/server.js
+++ door2life-backend/server.js
@@ -20,13 +20,15 @@ app.use(
   cors({
     origin: [
       "https://openingdoors2life.org",
-      "https://meek-swan-915441.netlify.app/",
+      "https://meek-swan-915441.netlify.app",
       "http://localhost:5173", // Local development
       "http://localhost:3000", // Alternative local port
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    preflightContinue: false,
+  optionsSuccessStatus: 204
   })
 );
 
