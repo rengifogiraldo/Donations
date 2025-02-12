1328b5d366e0ef0c8d3fd4b2b129dc0f46675367
kwagalanagroup <groupkwagalana@gmail.com>
2025-02-13 00:54:59 +0800

    add env docker

--- door2life-Frontend/vite.config.js
+++ door2life-Frontend/vite.config.js
@@ -12,7 +12,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://door2life-backend.vercel.app",
+        // target: "https://door2life-backend.vercel.app",
+        target: "http://localhost:8000",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path,
